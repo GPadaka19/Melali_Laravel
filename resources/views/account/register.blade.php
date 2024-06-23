1098dfd8a026b5079c2d577a1f1ca60c55e8ac9e
GPadaka19 <gustipadaka19@gmail.com>
2024-06-23 22:56:39 +0700

    fix css bug

--- resources/views/account/register.blade.php
+++ resources/views/account/register.blade.php
@@ -33,12 +33,9 @@
 </head>
 <body>
     <div class="bg-bgCustom flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-             <div class="flex flex-col items-center ">
             <a href="index.html" class="flex items-center text-2xl font-semibold text-gray-900 dark:text-white">
             <img src="img/logo.png" alt="logo" style="width: 199px; height: 66px; margin-bottom: 30px; margin-top: 30px; margin-right: 30px;">
-            <!-- Melali -->
             </a>
-        </div>
         <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
     <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
         <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
